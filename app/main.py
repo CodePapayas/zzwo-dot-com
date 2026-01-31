622091213485004f36bcc02b3c16e8f3bd40aaf4
CodePapayas <catspianos@gmail.com>
2026-01-30 22:45:07 -0800

    Add initial FastAPI application structure with templates and static files

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+async def home(request: Request):
+    nav_items = [
+        {"endpoint": "home", "text": "Home"},
+        # {"endpoint": "projects", "text": "Projects"}
+    ]
+    return templates.TemplateResponse(
+        "index.html", 
+        {"request": request, "nav_items": nav_items, "active_page": "home"}
+    )
